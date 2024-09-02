38d39dd5c1c36d2d62fbc01025187a0325fd3c14
OTAKUWeBer <137557891+OTAKUWeBer@users.noreply.github.com>
2024-09-02 21:15:31 +0600

    Initial commit of hangman game

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+from words import words  # Make sure words is a list of strings
+import string
+from hang_man_visual import lives_visual_dict
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while "-" in word or " " in word:
+        word = random.choice(words)
+        
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+    
+    lives = 6
+    
+    while len(word_letters) > 0 and lives > 0:
+        # Display used letters
+        print(f'You have used these letters: {", ".join(used_letters)}')
+        
+        # Display current progress
+        word_list = [letter if letter in used_letters else "_" for letter in word]
+        print(lives_visual_dict[lives])
+        print('Current word: ', ' '.join(word_list))
+        
+        # Getting user input
+        user_input = input("Guess a letter (or multiple letters): ").upper()
+        
+        for user_letter in user_input:
+            if user_letter in alphabet - used_letters:
+                used_letters.add(user_letter)
+                if user_letter in word_letters:
+                    word_letters.remove(user_letter)
+                    print(f"Correct guess for '{user_letter}'!")
+                
+                else:
+                    lives -= 1
+                    print(f"Incorrect guess for '{user_letter}'. You have {lives} lives left.")
+            
+            elif user_letter in used_letters:
+                print(f"You've already guessed '{user_letter}'.")
+            
+            else:
+                print(f"'{user_letter}' is an invalid character.")
+        
+        print()  # Add a blank line for readability between rounds
+    
+    if lives == 0:
+        print(lives_visual_dict[lives])
+        print(f"You died! The word was {word}.")
+    else:
+        print(f"Congratulations! You guessed the word: {word}")
+
+if __name__ == "__main__":
+    hangman()
